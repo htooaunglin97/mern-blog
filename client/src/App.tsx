a5960d94cc250d09dde7c2d3066a36b47ef18f31
htooaunglin97 <htooaunglin0297@gmail.com>
2024-11-03 14:52:41 +0630

    create pages and routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,22 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import About from "./pages/About.tsx";
+import Dashboard from "./pages/Dashboard.tsx";
+import SignIn from "./pages/SignIn.tsx";
+import SignUp from "./pages/SignUp.tsx";
+
 const App = () => {
     return (
         <div>
-            <h1 className="text-red-800">App</h1>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={< Home/>} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/sign-in" element={<SignIn />} />
+                    <Route path="/sign-up" element={<SignUp />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 };
