4f12cd1fd1276f0113f6ebf138a28a9bad3982ae
htooaunglin97 <htooaunglin0297@gmail.com>
2024-11-03 17:54:10 +0630

    modify the header section problem where links are not clickable in small screen sizes

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,50 +4,65 @@ import {AiOutlineSearch} from "react-icons/ai";
 import {FaMoon} from "react-icons/fa";
 
 const Header = () => {
-
   const path = useLocation().pathname;
+
   return (
     <Navbar className="border-b-2 my-2 pb-2" fluid>
-        <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
+      <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
           <span className="py-1 px-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
               Htoo's
           </span>
-          Blog
-        </Link>
-        <form>
-          <TextInput
-            type="text"
-            placeholder="Search blogs..."
-            rightIcon={AiOutlineSearch}
-            className="hidden lg:inline"
-          />
-        </form>
+        Blog
+      </Link>
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Search blogs..."
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        />
+      </form>
 
-        <Button className="lg:hidden" pill color="gray">
-          <AiOutlineSearch />
+      <Button className="lg:hidden" pill color="gray">
+        <AiOutlineSearch />
+      </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button className="hidden sm:inline" color="gray" pill>
+          <FaMoon />
         </Button>
-        <div className="flex gap-2 md:order-2">
-          <Button className="hidden sm:inline" color="gray" pill>
-            <FaMoon />
+        <Link to="sign-in">
+          <Button gradientDuoTone='purpleToBlue' pill>
+            Sign In
           </Button>
-          <Link to="sign-in">
-            <Button gradientDuoTone='purpleToBlue' pill>
-              Sign In
-            </Button>
-          </Link>
-          <Navbar.Toggle />
-        </div>
-        <Navbar.Collapse>
-          <Navbar.Link active={path === "/"} as={'div'}>
-            <Link to="/">Home</Link>
-          </Navbar.Link>
-          <Navbar.Link active={path === "/about"} as={'div'}>
-            <Link to="/about">About</Link>
-          </Navbar.Link>
-          <Navbar.Link active={path === "/projects"} as={'div'}>
-            <Link to="/projects">Projects</Link>
-          </Navbar.Link>
-        </Navbar.Collapse>
+        </Link>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link
+          as={Link}
+          to="/"
+          active={path === "/"}
+          className="block lg:inline-block w-full lg:w-auto"
+        >
+          Home
+        </Navbar.Link>
+        <Navbar.Link
+          as={Link}
+          to="/about"
+          active={path === "/about"}
+          className="block lg:inline-block w-full lg:w-auto"
+        >
+          About
+        </Navbar.Link>
+        <Navbar.Link
+          as={Link}
+          to="/projects"
+          active={path === "/projects"}
+          className="block lg:inline-block w-full lg:w-auto"
+        >
+          Projects
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
