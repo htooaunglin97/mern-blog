5cfce2dee29e290172bcdf93363d19a44b6a7eba
htooaunglin97 <htooaunglin0297@gmail.com>
2024-11-03 22:57:18 +0630

    create and test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
@@ -17,7 +18,9 @@ connectDB();
 
 const app = express();
 const port = 5000
+
 app.listen(5000, () => {
   console.log(`Server is running on port ${port}`);
-  
 })
+
+app.use('/api/users', userRoutes);
