2a0879f918437c65ac8f2f1dda1fdd76cf466587
htooaunglin97 <htooaunglin0297@gmail.com>
2024-11-03 20:00:57 +0630

    connect to the databse

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_STRING);
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+};
+
+connectDB();
 
 const app = express();
 const port = 5000
