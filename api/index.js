fcd731d8da4776127f420f8f37a3a2a67f0565f9
htooaunglin97 <htooaunglin0297@gmail.com>
2024-11-04 12:28:24 +0630

    add a middleware and a function to handle errors

--- api/index.js
+++ api/index.js
@@ -28,3 +28,13 @@ app.listen(5000, () => {
 
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
